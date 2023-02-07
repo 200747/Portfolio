619670ba394b244d0fb284764e98fdc84c2ee368
200747 <200747@student.glu.nl>
2023-02-07 19:37:26 +0100

    gfdgfgdfg

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,21 +9,47 @@ import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
+import Php from '../assets/php.png';
 
 const Skills = () => {
   return (
-    <div name='skills'>
-        <div>
+    <div name='skills' className='w-full h-screen bg-[#21223e] text-gray-300'>
+      {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p>Experience</p>
-                <p>// These are the projects I've worked with.</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+                <p className='py-4'>// These are the projects I've worked with</p>
             </div>
-            <div>
-                <img src={HTML} alt="" />
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-9 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                <p className='my-4'>HTML</p>
             </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+                <p className='my-4'>CSS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
+                <p className='my-4'>JAVASCRIPT</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                <p className='my-4'>REACT</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+                <p className='my-4'>GITHUB</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Php} alt="HTML icon" />
+                <p className='my-4'>PHP</p>
+            </div>
+          </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Skills
