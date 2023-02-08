0f5d9fca1dc7a59a232d6b293567d9f2df881dbf
200747 <200747@student.glu.nl>
2023-02-08 19:09:33 +0100

    lalala

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#21223e] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - johan.ton2003@hotmail.com</p>
+            </div>
+            <input className='bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6]' name="message" rows="10"></textarea>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
