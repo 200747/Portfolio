b564b0ad4aa564aaabc130e8e50dcc29d2a6d922
200747 <200747@student.glu.nl>
2023-02-15 17:51:48 +0100

    huhuhuhhu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -12,7 +13,8 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a web developer.</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a web developer based in The Netherlands. I'm focused on building responsive full-stack websites.  My primary skills are working with front-end.</p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View work
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <Link to="work" smooth={true} duration={500}>View work</Link>
           <span className='group-hover:rotate-90 duration-300'>
           <HiArrowNarrowRight className='ml-3' />
           </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,26 +87,26 @@ const Navbar = () => {
         <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://www.linkedin.com/in/johan-ton-a20a89246/" target="_blank">
                     Linkedin <FaLinkedin size={30} />
                 </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://github.com/200747" target="_blank">
                     Github <FaGithub size={30} />
                 </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="mailto:johan.ton2003@hotmail.com" target="_blank">
                     Email <HiOutlineMail size={30} />
                 </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
-                    Resume <BsFillPersonLinesFill size={30} />
+                href="https://resume.io/r/pIXDoQBkw" target="_blank">
+                    CV <BsFillPersonLinesFill size={30} />
                 </a>
             </li>
         </ul>
