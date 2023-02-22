c9965f814cd9c6daa67f5fcd5cb6f74d38a1d3c4
Johan Ton <200747@student.glu.nl>
2023-02-22 15:02:59 +0100

    dadasdfas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-[107vh] bg-[#21223e] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-[120vh] bg-[#21223e] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/19d11b84-b0b1-42b5-a2e9-3dbf888287d2" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import realEstate from '../assets/realestate.jpg'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full py-4 md:h-[120vh] text-gray-300 bg-[#21223e]'>
+    <div name='work' className='w-full py-4 md:h-[107vh] text-gray-300 bg-[#21223e]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
@@ -14,10 +14,10 @@ const Work = () => {
             <div 
 
             className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:opacity-100'>
               
 
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                             React JS Application
                         </span>
@@ -34,7 +34,7 @@ const Work = () => {
                 <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
-              <div className='opacity-0 group-hover:opacity-100'>
+                <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                   <span className='text-2xl font-bold text-white tracking-wider'>
                       React JS Application
                   </span>
@@ -52,7 +52,7 @@ const Work = () => {
                           <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                             React JS Application
                         </span>
@@ -69,7 +69,7 @@ const Work = () => {
                 <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
-              <div className='opacity-0 group-hover:opacity-100'>
+                <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                   <span className='text-2xl font-bold text-white tracking-wider'>
                       React JS Application
                   </span>
@@ -87,7 +87,7 @@ const Work = () => {
           <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
-              <div className='opacity-0 group-hover:opacity-100'>
+          <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                   <span className='text-2xl font-bold text-white tracking-wider'>
                       React JS Application
                   </span>
@@ -104,7 +104,7 @@ const Work = () => {
           <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
         
 
-        <div className='opacity-0 group-hover:opacity-100'>
+          <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
             <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
             </span>
