2a2757fc03089c6c818831220a4a0158a661c973
200747 <200747@student.glu.nl>
2023-03-02 22:49:14 +0100

    eeee

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import NetflixImg from '../assets/netflixpic.PNG'
+import ColorImg from '../assets/colorpickerpic.PNG'
+import VoiceImg from '../assets/voicepic.PNG'
 import realEstate from '../assets/realestate.jpg'
 import WorkImg from '../assets/workImg.jpeg'
+
 
 const Work = () => {
   return (
@@ -32,36 +35,36 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${ColorImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
                 <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                   <span className='text-2xl font-bold text-white tracking-wider'>
-                      React JS Application
+                      Color Picker In JS
                   </span>
                   <div className='pt-8 text-center'>
-                      <a href="/">
+                      <a href="https://color-picker-tawny-iota.vercel.app/" target="_blank">
                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                       </a>
-                      <a href="/">
+                      <a href="https://github.com/200747/color-picker" target="_blank">
                           <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                       </a>
 
                   </div>
               </div>
           </div>
-                          <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                          <div style={{backgroundImage: `url(${VoiceImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
               
 
                     <div className='opacity-0 group-hover:opacity-100 transition-all duration-500'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Voice Changer In JS
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://voicechanger.vercel.app/" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/200747/voicechanger" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
